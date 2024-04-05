b1da87ce51589ca20746ebae8ad9a8fdaeb50ee5
apyd <arturpydych@gmail.com>
2024-04-05 15:56:49 +0200

    chore: remove inmemory db and move utils into api

--- src/server.ts
+++ src/server.ts
@@ -5,7 +5,6 @@ import router from "./api/routes";
 
 const { DB_APP_NAME, DB_APP_USER, DB_APP_PASSWORD, DB_SERVER, DB_PORT, APP_PORT } = process.env
 const DB_URI = `mongodb://${DB_APP_USER}:${DB_APP_PASSWORD}@${DB_SERVER}:${DB_PORT}/${DB_APP_NAME}`
-console.log(DB_URI)
 
 const app = express();
 
