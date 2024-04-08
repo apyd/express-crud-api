2a60b447b1c2c05bffa93954540000d6b8ec4398
apyd <arturpydych@gmail.com>
2024-04-08 15:09:21 +0200

    feat: use id instead of _id for returned products

--- src/api/products/products.model.ts
+++ src/api/products/products.model.ts
@@ -8,6 +8,18 @@ export const productSchema = new mongoose.Schema({
   title: { type: String, required: true},
   description: {type: String, required: true},
   price: { type: Number, required: true},
+}, {
+  toJSON: {
+    virtuals: true,
+    transform: function(doc, ret) {
+      // use id instead of _id in output
+      ret.id = doc._id;
+
+      // remove fields that skipped in response
+      delete ret._id;
+      delete ret.__v;
+    }
+  }
 })
 
 const ProductModel = mongoose.model<Product>("Product", productSchema)
