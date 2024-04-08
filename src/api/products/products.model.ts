212a7aa5244c41a4995a6fa2efbf97e883d4f027
apyd <arturpydych@gmail.com>
2024-04-08 12:26:15 +0200

    fix: set product fields as required

--- src/api/products/products.model.ts
+++ src/api/products/products.model.ts
@@ -1,12 +1,13 @@
 import mongoose from "mongoose";
+import crypto from "node:crypto";
 
 import type { Product } from "./product/product.types";
 
 export const productSchema = new mongoose.Schema({
-  id: mongoose.Schema.Types.UUID,
-  title: String,
-  description: String,
-  price: Number
+  _id: { type: String, default: crypto.randomUUID },
+  title: { type: String, required: true},
+  description: {type: String, required: true},
+  price: { type: Number, required: true},
 })
 
 const ProductModel = mongoose.model<Product>("Product", productSchema)
