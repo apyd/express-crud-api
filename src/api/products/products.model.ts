5f4801d6d6c7e564991bc9db33ee17596ebad58a
apyd <arturpydych@gmail.com>
2024-04-27 21:11:04 +0200

    refactor: update Products model with Model types

--- src/api/products/products.model.ts
+++ src/api/products/products.model.ts
@@ -1,8 +1,16 @@
-import { DataTypes, Model } from "sequelize";
+import { DataTypes, Model, InferAttributes, InferCreationAttributes, CreationOptional } from "sequelize";
 import { sequelize } from "../../server";
 import type { Product } from "./product/product.types";
+import { UUID } from "node:crypto";
 
-const ProductModel = sequelize.define<Model<Product, Omit<Product, "id">>>('Product', {
+interface ProductModel extends Model<InferAttributes<ProductModel>, InferCreationAttributes<ProductModel>> {
+  id: CreationOptional<UUID>;
+  title: Product['title'],
+  description: Product['description'],
+  price: Product['price']
+}
+
+const ProductModel = sequelize.define<ProductModel>('Product', {
   id: { type: DataTypes.UUID, primaryKey: true, defaultValue: DataTypes.UUIDV4 },
   title: { type: DataTypes.STRING, allowNull: false },
   description: { type: DataTypes.STRING, allowNull: false },
