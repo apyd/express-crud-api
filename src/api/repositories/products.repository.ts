f822541b03e48f5cd029b417763cecd06680403a
apyd <arturpydych@gmail.com>
2024-05-06 10:41:00 +0200

    fix: omit timestamps in product response

--- src/api/repositories/products.repository.ts
+++ src/api/repositories/products.repository.ts
@@ -2,7 +2,7 @@ import Product from "../models/products.model";
 import { timestampsFieldsList } from "../constants";
 import type { ProductId } from "../types/product.types";
 
-export const getAll = async () => await Product.findAll()
+export const getAll = async () => await Product.findAll({attributes: { exclude: timestampsFieldsList }})
 
 export const getById = async (productId: ProductId) => await Product.findByPk(productId, {
     attributes: { exclude: timestampsFieldsList }
