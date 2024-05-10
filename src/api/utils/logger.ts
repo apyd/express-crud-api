f2be632bdaa1ce56f6467bab612e0b9e60d04209
apyd <arturpydych@gmail.com>
2024-05-10 10:51:25 +0200

    feat: add winston logger util

---
+++ src/api/utils/logger.ts
@@ -0,0 +1,10 @@
+import winston from "winston";
+
+export const logger = winston.createLogger({
+    level: 'info',
+    format: winston.format.json(),
+    transports: [
+      new winston.transports.Console(),
+      new winston.transports.File({ filename: 'error.log', level: 'error' })
+    ]
+  });
